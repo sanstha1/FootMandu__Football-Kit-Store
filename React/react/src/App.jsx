ee87ca1841f2a3ad63a032539db9bffe14d0febe
Santosh Shrestha <sthasantosh070@gmail.com>
2025-01-09 14:18:29 +0545

    New Changes done

--- React/react/src/App.jsx
+++ React/react/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+const Login = lazy(() => import('./components/public/login.jsx'));
+// const SimpleForm = lazy(()=> import('./components/public/Form.jsx'));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        {/* <Route path='/' element={<SimpleForm/>}/> */}
+      </Routes>
+      </Suspense>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
+
---
+++ React/react/src/components/public/Login.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import '../css/login.css';
+import { Link } from 'react-router-dom';
+
+function Login() {
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="image-container">
+          <img src="/Images/logo3.png" alt="logo" />
+        </div>
+        <ul>
+          <li>Home</li>
+        </ul>
+        <button type="submit" className='buttonsignup'>SIGN UP</button>
+        <div className="user">
+          <img src="/Images/icons8-user-48.png" alt="user icon" />
+        </div>
+      </div>
+      <div className="couple">
+        <img src="/Images/Image.png" alt="couple" />
+      </div>
+      <div className="form">
+        <form>
+        {/* <form onSubmit={(e) => e.preventDefault()}> */}
+          <h1>LOG IN</h1>
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            className="input"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            className="input"
+          />
+          <div className="remember-forgot">
+            <label><input type="checkbox" /> Remember me</label>
+            <a>Forgot Password?</a>
+          </div>
+          <button type="submit" className="btn">LOG IN</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
